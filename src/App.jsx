8a1c2106fde6eeb4179f22d226ae00e92eb03241
Diaa-E <blarb21@yahoo.com>
2024-02-10 22:31:46 +0200

    Pass initial dark mode value from main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import "./styles/reset.css";
 import './styles/App.css';
-import "./styles/theme";
+import "./styles/theme.css";
 import { isSmallScreen, toggleMobileMode } from './mobileMode';
 
-function App() {
+function App({useDarkMode}) {
   const [mobileMode, mobileModeSetter] = useState(isSmallScreen());
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(useDarkMode);
 
   toggleMobileMode(mobileModeSetter);
 
