68526e7eeb0c2d2b76ac4c6baa925071478468ef
Diaa-E <blarb21@yahoo.com>
2024-02-10 22:28:14 +0200

    Add dark mode state - Move repeated css to a common class

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 import "./styles/reset.css";
-import './styles/App.css'
+import './styles/App.css';
+import "./styles/theme";
 import { isSmallScreen, toggleMobileMode } from './mobileMode';
 
 function App() {
   const [mobileMode, mobileModeSetter] = useState(isSmallScreen());
+  const [darkMode, setDarkMode] = useState(false);
 
   toggleMobileMode(mobileModeSetter);
 
   return (
-    <>
-    </>
+    <div className={`"common" ${darkMode? "dark" : "light"}`}>
+
+    </div>
   )
 }
 
