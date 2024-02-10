e41c9aba56fe72c3371a906081e97f0ec5f100ad
Diaa-E <blarb21@yahoo.com>
2024-02-10 22:21:06 +0200

    Add a mobileMode state

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import "./styles/reset.css";
 import './styles/App.css'
+import { isSmallScreen, toggleMobileMode } from './mobileMode';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mobileMode, mobileModeSetter] = useState(isSmallScreen());
+
+  toggleMobileMode(mobileModeSetter);
 
   return (
     <>
