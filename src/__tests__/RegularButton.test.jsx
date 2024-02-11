22022dc3485d2c1812093f7ac70004ce953546c7
Diaa-E <blarb21@yahoo.com>
2024-02-12 01:58:10 +0200

    Add click handlers to regular button component

--- src/__tests__/RegularButton.test.jsx
+++ src/__tests__/RegularButton.test.jsx
@@ -1,8 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import RegularButton from "../components/RegularButton";
+import userEvent from "@testing-library/user-event";
+import { vi } from "vitest";
 
 describe("RegularButton component", () => {
+
+    function setup(jsx)
+    {
+        return {
+            user: userEvent.setup(),
+            ...render(jsx)
+        };
+    }
 
     it("Renders correctly", () => {
 
@@ -38,5 +48,23 @@ describe("RegularButton component", () => {
         render(<RegularButton danger={false}/>);
 
         expect(screen.getByRole("button")).not.toHaveClass("button-danger");
+    });
+
+    it("Does not call callback when it is not clicked", () => {
+
+        const onClick = vi.fn();
+        const {user} = setup(<RegularButton onClick={onClick}/>);
+
+        expect(onClick).not.toHaveBeenCalled();
+    });
+
+    it("Calls callback when it is clicked", async () => {
+
+        const onClick = vi.fn();
+        const {user} = setup(<RegularButton onClick={onClick}/>);
+        const button = screen.getByRole("button");
+        await user.click(button);
+
+        expect(onClick).toHaveBeenCalled();
     });
 });
--- src/components/RegularButton.jsx
+++ src/components/RegularButton.jsx
@@ -1,7 +1,7 @@
-export default function RegularButton({text, iconPath, danger})
+export default function RegularButton({text, iconPath, danger, onClick})
 {
     return (
-        <button className={`${danger ? "button-danger" : ""}`}>
+        <button onClick={onClick} className={`${danger ? "button-danger" : ""}`}>
             <img src={iconPath} alt={`${text} button icon`} />
             {text}
         </button>
