bc840de562f16e9bef0f1c8fe68c3cdeae940c3e
Diaa-E <blarb21@yahoo.com>
2024-02-12 00:34:48 +0200

    Remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import "./styles/reset.css";
 import './styles/App.css';
 import "./styles/theme.css";
