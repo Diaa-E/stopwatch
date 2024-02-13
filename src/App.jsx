5a57abdaa5bdba842f01e48bc97c4b79ccadef63
Diaa-E <blarb21@yahoo.com>
2024-02-14 01:45:03 +0200

    Append navBar to the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,34 @@ import "./styles/reset.css";
 import './styles/App.css';
 import "./styles/theme.css";
 import { isSmallScreen, toggleMobileMode } from './mobileMode';
+import NavBar from './components/NavBar';
+import { tabs } from './tabs';
 
 function App({useDarkMode}) {
+  
   const [mobileMode, setMobileMode] = useState(isSmallScreen());
   const [darkMode, setDarkMode] = useState(useDarkMode);
+  const [activeTab, setActiveTab] = useState(tabs.stopwatch);
 
   toggleMobileMode(setMobileMode);
 
+  function toggleDarkMode()
+  {
+    setDarkMode(darkMode => !darkMode);
+  }
+
+  function changeActiveTab(tabId)
+  {
+    setActiveTab(tabId);
+  }
+
   return (
     <div id='main' role='main' className={`common ${darkMode? "dark" : "light"}`}>
-
+      <NavBar
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode}
+        changeActiveTab={changeActiveTab}
+      />
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,32 @@
-export default function NavBar({})
+import TabButton from "./TabButton";
+import { iconBarrel } from "../iconBarrel";
+import { tabs } from "../tabs";
+
+export default function NavBar({changeActiveTab, darkMode, toggleDarkMode})
 {
     return (
         <>  
-            <nav>
-                
+            <nav className="nav-bar">
+                <TabButton
+                    iconPath={iconBarrel.stopwatch}
+                    text={"Stopwatch"}
+                    onClick={() => changeActiveTab(tabs.stopwatch)}
+                />
+                <TabButton
+                    iconPath={iconBarrel.timer}
+                    text={"Timer"}
+                    onClick={() => changeActiveTab(tabs.timer)}
+                />
+                <TabButton
+                    iconPath={darkMode ? iconBarrel.light : iconBarrel.dark}
+                    text={darkMode ? "Light" : "Dark"}
+                    onClick={toggleDarkMode}
+                />
+                <TabButton
+                    iconPath={iconBarrel.about}
+                    text={"About"}
+                    onClick={() => changeActiveTab(tabs.about)}
+                />
             </nav>
         </>
     )
