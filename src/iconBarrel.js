dfc997fbefd5c697d3486f57d8edeee4c51bde56
Diaa-E <blarb21@yahoo.com>
2024-02-14 01:43:16 +0200

    Add app icons

---
+++ src/iconBarrel.js
@@ -0,0 +1,24 @@
+import aboutIcon from "./assets/icons/about.svg";
+import cancelIcon from "./assets/icons/cancel.svg";
+import darkIcon from "./assets/icons/dark.svg";
+import lapIcon from "./assets/icons/lap.svg";
+import lightIcon from "./assets/icons/light.svg";
+import pauseIcon from "./assets/icons/pause.svg";
+import resetIcon from "./assets/icons/reset.svg";
+import startIcon from "./assets/icons/start.svg";
+import stopwatchIcon from "./assets/icons/stopwatch.svg";
+import timerIcon from "./assets/icons/timer.svg";
+
+export const iconBarrel = {
+    
+    about: aboutIcon,
+    cancel: cancelIcon,
+    dark: darkIcon,
+    lap: lapIcon,
+    light: lightIcon,
+    pause: pauseIcon,
+    reset: resetIcon,
+    start: startIcon,
+    stopwatch: stopwatchIcon,
+    timer: timerIcon,
+}
