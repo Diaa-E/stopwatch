2f9b31e2fa52074835e705ccda7948de71ede1a3
Diaa-E <blarb21@yahoo.com>
2024-02-15 12:26:42 +0200

    Correct timer input before submiting

--- src/components/TimerSetter.jsx
+++ src/components/TimerSetter.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import RegularButton from "./RegularButton";
+import { convertToDoubleDigit } from "../timeConverter";
 
 export default function TimerSetter({formattedTime, onSubmit})
 {
@@ -7,15 +8,20 @@ export default function TimerSetter({formattedTime, onSubmit})
     const [mins, setMins] = useState(formattedTime[1]);
     const [secs, setSecs] = useState(formattedTime[2]);
 
+    function correctValue(e, upperLimit)
+    {
+        if (+e.target.value > upperLimit) return upperLimit;
+        if (+e.target.value < 0 || e.target.value === "") return "00";
+        return convertToDoubleDigit(e.target.value);
+    }
+
     return (
         <form onSubmit={onSubmit} role="form" action="">
             <div>
                 <input
                     id="hours"
-                    maxLength={2}
-                    minLength={2}
-                    required
                     onChange={e => setHrs(e.target.value)}
+                    onBlur={e => setHrs(correctValue(e, 99))}
                     role="input"
                     type="number"
                     value={hrs}
@@ -23,10 +29,8 @@ export default function TimerSetter({formattedTime, onSubmit})
                 />
                 <input
                     id="minutes"
-                    maxLength={2}
-                    minLength={2}
-                    required
                     onChange={e => setMins(e.target.value)}
+                    onBlur={e => setMins(correctValue(e, 59))}
                     role="input"
                     type="number"
                     value={mins}
@@ -34,10 +38,8 @@ export default function TimerSetter({formattedTime, onSubmit})
                 />
                 <input
                     id="seconds"
-                    maxLength={2}
-                    minLength={2}
-                    required
                     onChange={e => setSecs(e.target.value)}
+                    onBlur={e => setSecs(correctValue(e, 59))}
                     role="input"
                     type="number"
                     value={secs}
