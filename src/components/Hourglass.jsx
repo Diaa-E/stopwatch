fa4e3ffbdf38e057ba601c5703cfa118eb547ae0
Diaa-E <blarb21@yahoo.com>
2024-02-17 06:29:04 +0200

    Use transform for hourglass animation to reduce repaint

--- src/components/Hourglass.jsx
+++ src/components/Hourglass.jsx
@@ -8,13 +8,13 @@ export default function Hourglass({time, timeTarget})
     return (
         <div role="timer" className="hg-container">
             <div data-testid="hourglassTop" className="hg-common hg-top">
-                <div style={{height: `${sandHeight.top}%`}} data-testid="sandTop" className="hg-inner">
+                <div style={{transform: `scaleY(${sandHeight.top}%)`}} data-testid="sandTop" className="hg-inner">
 
                 </div>
             </div>
             <p data-testid="time" className="hg-time" >{convertFromSeconds(time).join(":")}</p>
             <div data-testid="hourglassBottom" className="hg-common hg-bottom">
-                <div style={{height: `${sandHeight.bottom}%`}} data-testid="sandBottom" className="hg-inner">
+                <div style={{transform: `scaleY(${sandHeight.bottom}%)`}} data-testid="sandBottom" className="hg-inner">
 
                 </div>
             </div>
